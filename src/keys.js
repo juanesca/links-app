2996aa0805511e98eccdd40992cbf13e8d6acf59
juanesca <juanesca2207@gmail.com>
2021-02-02 15:24:45 -0500

    links

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'b7hwpcga9wqj6x5leufo-mysql.services.clever-cloud.com',
+        user: 'uimvoulzodqh2lq2',
+        password: '9s59qgmPTsam7i8vjUbg',
+        database: 'b7hwpcga9wqj6x5leufo'
+    }
+};
